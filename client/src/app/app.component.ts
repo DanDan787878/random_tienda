89752bd16458e23798856e224d4221092a09ef25
takexhi <eientakeshi10@gmail.com>
2025-03-06 14:51:29 -0500

    Se integró bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderCComponent } from "./client/header-c/header-c.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderCComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
